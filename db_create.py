4568bcfdf87ff5ac455fc37afe1192269ba4c936
Jonny Perrin <jonathan.w.perrin@gmail.com>
2019-04-08 21:11:31 -0400

    changed dbs

--- db_create.py
+++ db_create.py
@@ -1,19 +1,13 @@
-import sqlite3
-from _config import DATABASE_PATH
+#  db_create.py
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-    c = connection.cursor()
+from views import db
+from models import Task
+from datetime import date
 
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-        status INTEGER NOT NULL)""")
+db.create_all()
 
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
-        )
 
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish this tutorial", "03/25/2015", 10, 1)'
-        )
+db.session.add(Task("Finish this tutorial", date(2016, 9, 22), 10, 1))
+db.session.add(Task("Finish Real Python", date(2016, 10, 3), 10, 1))
+
+db.session.commit()
